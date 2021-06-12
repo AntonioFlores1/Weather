8cd258365fce75fca983dfd1406abd5adbba853b
Antonio Flores <johndoe@johns-mbp.home>
2021-06-12 00:30:03 -0400

    Set up Network Manager and App Error for related issues

---
+++ Weather/MVC/App Error/AppError.swift
@@ -0,0 +1,20 @@
+//
+//  AppError.swift
+//  Weather
+//
+//  Created by Antonio Flores on 6/12/21.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case unauthenticated
+    case invalidJSONResponse
+    case couldNotParseJSON(rawError: Error)
+    case noInternetConnection
+    case badURL
+    case badStatusCode
+    case noDataReceived
+    case notAnImage
+    case other(rawError: Error)
+}
